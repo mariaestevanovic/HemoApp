f949121ab0592fb95abe0bb330b7fc36f30020e3
mclaraoem <mcestevanovic@gmail.com>
2024-11-21 19:30:14 -0300

    script banco

---
+++ criacaobanco.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE hemoapp_db;
+USE hemoapp_db;
+
+CREATE TABLE Users (
+  Id INT PRIMARY KEY AUTO_INCREMENT,
+  FullName VARCHAR(100),
+  Email VARCHAR(100),
+  Password VARCHAR(100),
+  Gender VARCHAR(10)
+);
+
+SELECT * FROM Users;
