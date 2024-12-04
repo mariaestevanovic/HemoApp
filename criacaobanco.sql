8d312185815d9e10da3405fe19c353c544691664
mclaraoem <mcestevanovic@gmail.com>
2024-12-04 19:37:19 -0300

    ajuste query banco

--- criacaobanco.sql
+++ criacaobanco.sql
@@ -1,10 +1,19 @@
-CREATE DATABASE hemoapp_db;
-USE hemoapp_db;
+CREATE DATABASE hemoapp;
 
-CREATE TABLE Users (
-  Id INT PRIMARY KEY AUTO_INCREMENT,
-  FullName VARCHAR(100) NOT NULL,
-  Email VARCHAR(100) NOT NULL,
-  Password VARCHAR(100) NOT NULL,
-  Gender ENUM('Masculino', 'Feminino') NOT NULL
+USE hemoapp;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  phone VARCHAR(15),
+  location VARCHAR(255),
+  blood_type VARCHAR(5),
+  birth_date DATE,
+  weight DECIMAL(5,2),
+  bio TEXT,
+  rg VARCHAR(20),
+  cpf VARCHAR(14),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
